a474bdf145dc3e6485ca74f0aea49ddce4e32a3a
Eric-Gordon <erickgordon96@gmail.com>
2019-03-13 20:25:45 -0400

    working thrc reating a nde tree

--- alphabeta.py
+++ alphabeta.py
@@ -1,98 +1,117 @@
-# Reads file
-def read_file(self):
-    with open(self.file, 'r') as f:
-        self.maze = [line.strip() for line in f]
-    self.width = len(self.maze[0])
-    self.height = len(self.maze)
-# print(self.maze)
-
-# Creates grid based on text file
+import re
 
 
-def create_grid(self):
-    for x in range(self.width):
-        for y in range(self.height):
-            if self.maze[y][x].lower() == 'x':
-                reachable = False
-            else:
-                reachable = True
-            self.cells.append(Cell(x, y, reachable))
-            if self.maze[y][x].lower() == 's':
-                self.start = self.check_cell(x, y)
-            if self.maze[y][x].lower() == 'g':
-                self.goal = self.check_cell(x, y)
+class Node(object):
+    def __init__(self, Name):
+        self.Name = Name
+        self.parent = None
+        self.children = []
+
+    def add(self, child):
+        self.children.append(child)
+        child.parent = self
 
 
-class AlphaBeta:
-    # print utility value of root node (assuming it is max)
-    # print names of all nodes visited during search
-    def __init__(self, game_tree):
-        self.game_tree = game_tree  # GameTree
-        self.root = game_tree.root  # GameNode
-        return
+def read_file(file):
+    with open(file, 'r') as f:
+        for line in f:
+            result = line[line.find(" "):]
+            result = result.replace('{', '')
+            result = result.replace('}', '')
+            result = result.replace(' ', '')
+            current_node = Node(result[1])
+            for i in range(len(result)-1, 0, -6):
+                print("1_node: "+str(result[i-3])+" 2_node: "+str(result[i-1]))
+                # next_node = Node(result[i+3])
+                # if result[i+1] == current_node.Name:
+                #     current_node.add(next_node)
+                # else:
+                #     current_node = find_parent(next_node.Name, current_node)
 
-    def alpha_beta_search(self, node):
-        infinity = float('inf')
-        best_val = -infinity
-        beta = infinity
 
-        successors = self.getSuccessors(node)
-        best_state = None
-        for state in successors:
-            value = self.min_value(state, best_val, beta)
-            if value > best_val:
-                best_val = value
-                best_state = state
-        print "AlphaBeta:  Utility Value of Root Node: = " + str(best_val)
-        print "AlphaBeta:  Best State is: " + best_state.Name
-        return best_state
+# def find_node(name, node):
+    # if node.Name == name:
+    #     return node
+    # else:
+    #     if node.parent not None:
+    #         return find_parent(name, node.parent)
 
-    def max_value(self, node, alpha, beta):
-        print "AlphaBeta-->MAX: Visited Node :: " + node.Name
-        if self.isTerminal(node):
-            return self.getUtility(node)
-        infinity = float('inf')
-        value = -infinity
 
-        successors = self.getSuccessors(node)
-        for state in successors:
-            value = max(value, self.min_value(state, alpha, beta))
-            if value >= beta:
-                return value
-            alpha = max(alpha, value)
-        return value
+read_file("./alphabeta.txt")
 
-    def min_value(self, node, alpha, beta):
-        print "AlphaBeta-->MIN: Visited Node :: " + node.Name
-        if self.isTerminal(node):
-            return self.getUtility(node)
-        infinity = float('inf')
-        value = infinity
 
-        successors = self.getSuccessors(node)
-        for state in successors:
-            value = min(value, self.max_value(state, alpha, beta))
-            if value <= alpha:
-                return value
-            beta = min(beta, value)
+# class AlphaBeta:
+#     # print utility value of root node (assuming it is max)
+#     # print names of all nodes visited during search
+#     def __init__(self, file):
+#         with open(file, 'r') as f:
+#             self.game_tree = [line.strip() for line in f]
+#         self.game_tree = game_tree  # GameTree
+#         self.root = game_tree.root  # GameNode
+#         return
 
-        return value
+#     def alpha_beta_search(self, node):
+#         infinity = float('inf')
+#         best_val = -infinity
+#         beta = infinity
 
-    #                     #
-    #   UTILITY METHODS   #
-    #                     #
+#         successors = self.getSuccessors(node)
+#         best_state = None
+#         for state in successors:
+#             value = self.min_value(state, best_val, beta)
+#             if value > best_val:
+#                 best_val = value
+#                 best_state = state
+#         print "AlphaBeta:  Utility Value of Root Node: = " + str(best_val)
+#         print "AlphaBeta:  Best State is: " + best_state.Name
+#         return best_state
 
-    # successor states in a game tree are the child nodes...
-    def getSuccessors(self, node):
-        assert node is not None
-        return node.children
+#     def max_value(self, node, alpha, beta):
+#         print "AlphaBeta-->MAX: Visited Node :: " + node.Name
+#         if self.isTerminal(node):
+#             return self.getUtility(node)
+#         infinity = float('inf')
+#         value = -infinity
 
-    # return true if the node has NO children (successor states)
-    # return false if the node has children (successor states)
-    def isTerminal(self, node):
-        assert node is not None
-        return len(node.children) == 0
+#         successors = self.getSuccessors(node)
+#         for state in successors:
+#             value = max(value, self.min_value(state, alpha, beta))
+#             if value >= beta:
+#                 return value
+#             alpha = max(alpha, value)
+#         return value
 
-    def getUtility(self, node):
-        assert node is not None
-        return node.value
+#     def min_value(self, node, alpha, beta):
+#         print "AlphaBeta-->MIN: Visited Node :: " + node.Name
+#         if self.isTerminal(node):
+#             return self.getUtility(node)
+#         infinity = float('inf')
+#         value = infinity
+
+#         successors = self.getSuccessors(node)
+#         for state in successors:
+#             value = min(value, self.max_value(state, alpha, beta))
+#             if value <= alpha:
+#                 return value
+#             beta = min(beta, value)
+
+#         return value
+
+#     #                     #
+#     #   UTILITY METHODS   #
+#     #                     #
+
+#     # successor states in a game tree are the child nodes...
+#     def getSuccessors(self, node):
+#         assert node is not None
+#         return node.children
+
+#     # return true if the node has NO children (successor states)
+#     # return false if the node has children (successor states)
+#     def isTerminal(self, node):
+#         assert node is not None
+#         return len(node.children) == 0
+
+#     def getUtility(self, node):
+#         assert node is not None
+#         return node.value
